846661628f54ee49a19433c95e57f563962618e6
nytrok1 <nicogiamarco@gmail.com>
2023-10-04 20:16:18 -0300

    Primer archivo de App

---
+++ AppNico/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    puesto = models.CharField(max_length=40)
+
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    documento = models.IntegerField()
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=40)
+    precio = models.IntegerField()
+
+
